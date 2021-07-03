08bc72e9a01df54eaadacc416a6345a734a8aac1
hogsmill <info@hogsmill.com>
2021-07-03 09:54:12 +0100

    initial version

---
+++ deploy.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+FORCE=false
+OUTDATED=true
+while [ $1 ]
+do
+  echo $1
+  if [ "$1" == "-f" ]; then
+    FORCE=true
+  fi
+  if [ "$1" == "-o" ]; then
+    OUTDATED=false
+  fi
+  shift
+done
+
+REPO="https://github.com/hogsmill/context-switching.git"
+APPS=(
+  'context-switching,contextSwitching,3003,Context Switching,Context Switching'
+  'requirements-game,requirement,3033,Requirements Game,Requirements Game'
+  'context-switching-guardian,contextSwitchingGuardian,3032,Context Switching,Context Switching'
+)
+
+for ((i = 0; i < ${#APPS[@]}; i++))
+do
+  REC="${APPS[$i]}"
+
+  APP=`echo $REC | cut -d, -f1`
+  COLLECTION=`echo $REC | cut -d, -f2`
+  PORT=`echo $REC | cut -d, -f3`
+  APPTYPE=`echo $REC | cut -d, -f4`
+  APPNAME=`echo $REC | cut -d, -f5`
+
+  echo "------------------------------------------------"
+  if [ -z "$APPNAME" ]; then
+    echo "Installing $APPTYPE:$APP ($COLLECTION, $PORT)"
+  else
+    echo "Installing $APPTYPE:$APP ($COLLECTION, $PORT, $APPNAME)"
+  fi
+  echo "------------------------------------------------"
+
+  DIR="/usr/apps/$APP"
+  if [ ! -d $DIR ]; then
+    git clone $REPO $DIR
+  fi
+  ENVFILE="$DIR/.env"
+  echo "VUE_APP_PORT=$PORT" > $ENVFILE
+  echo "VUE_APP_TYPE=$APPTYPE" >> $ENVFILE
+  echo "VUE_APP_COLLECTION=$COLLECTION" >> $ENVFILE
+  if [ ! -z "$APPNAME" ]; then
+    echo "VUE_APP_NAME=$APPNAME" >> $ENVFILE
+  fi
+
+  cd $DIR
+
+  PWD=`pwd`
+  APP=`basename $PWD`
+  git stash
+  GIT=`git pull`
+  echo $GIT
+  if [ "$FORCE" != "true" -a "$GIT" == "Already up to date." ]; then
+    exit 0
+  fi
+
+  npm install --legacy-peer-deps
+  npm run build
+  if [ ! -d /var/www/html/$APP/ ]; then
+    mkdir /var/www/html/$APP
+  fi
+  if [ -d /var/www/html/$APP/css ]; then
+    rm /var/www/html/$APP/css/*
+  else
+    mkdir /var/www/html/$APP/css
+  fi
+  if [ -d /var/www/html/$APP/js ]; then
+    rm /var/www/html/$APP/js/*
+  else
+    mkdir /var/www/html/$APP/js
+  fi
+  cp -R dist/* /var/www/html/$APP
+  if [ -f "src/server.js" ]; then
+    SERVER=`ps -ef | grep server.js | grep "/$APP/" | awk {'print $2'}`
+    if [ "$SERVER" != "" ]; then
+      kill -9 $SERVER
+    fi
+  fi
+
+done
+
+if [ "$OUTDATED" == "true" ]; then
+  php /usr/apps/monitor/src/lib/outdated.php &
+fi
